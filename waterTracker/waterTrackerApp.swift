c15638715d93dcaa75fc2b86a52b101eaa543d9b
lujin <lujinmohammed223@gmail.com>
2024-09-26 17:21:21 +0300

    Initial Commit

---
+++ waterTracker/waterTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  waterTrackerApp.swift
+//  waterTracker
+//
+//  Created by lujin mohammed on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct waterTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
